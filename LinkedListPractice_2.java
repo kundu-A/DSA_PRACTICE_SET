d20c1fba36e3bda350c50e19fdffb8485c666e87
Kundu-A <kunduarpan43@gmail.com>
2024-09-23 18:01:01 +0530

    Five new methods are added

--- LinkedListPractice_2.java
+++ LinkedListPractice_2.java
@@ -49,6 +49,23 @@ class LinkedList{
         prev.next=temp.next;
         System.out.println("One node is removed successfully.");
     }
+    public void reversePrint(){
+        if(head==null){
+            System.out.println("The list is empty.");
+            return;
+        }
+        Node tempHead=head;
+        Node nextPointer=null;
+        Node previous=null;
+        while (tempHead!=null) {
+            nextPointer=tempHead.next;
+            tempHead.next=previous;
+            previous=tempHead;
+            tempHead=nextPointer;
+        }
+        head=previous;
+        return;
+    }
     public void display(){
         if(head==null){
             System.out.println("The list is empty.");
@@ -61,6 +78,68 @@ class LinkedList{
         }
         System.out.println();
     }
+    public void searchElement(int data){
+        Node temp=head;
+        if(head==null){
+            System.out.println("The list is empty.");
+            return;
+        }
+        int count=0,flag=0;
+        while (temp!=null) {
+            count++;
+            if(temp.data==data){
+                System.out.println(data+" is found at "+count);
+                flag=1;
+                break;
+            }
+            temp=temp.next;
+        }
+        if(flag==0)
+            System.out.println(data+" is not found.");
+    }
+    public void measureSize(){
+        Node temp=head;
+        int count=0;
+        while (temp!=null) {
+            count++;
+            temp=temp.next;
+        }
+        System.out.println("The size of the list is: "+count);
+    }
+    public void getFirstAndLastElement(){
+        Node temp=head;
+        if(temp==null){
+            System.out.println("The list is empty.");
+            return;
+        }
+        System.out.println("First element of this list is: "+temp.data);
+        while (temp.next!=null) {
+            temp=temp.next;
+        }
+        System.out.println("Last element of this list is: "+temp.data);
+    }
+    public void middleElement(){
+        if (head==null) {
+            System.out.println("The list is empty.");
+            return;
+        }
+        Node slow=head;
+        Node fast=head;
+        while (fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        System.out.println("The middle element is: "+slow.data);
+    }
+    public void clearList(){
+        if(head==null){
+            System.out.println("the List is empty.");
+            return;
+        }
+        head.next=null;
+        head=null;
+        display();
+    }
 }
 public class LinkedListPractice_2 {
     public static void main(String[] args) {
@@ -68,7 +147,7 @@ public class LinkedListPractice_2 {
         Scanner sc=new Scanner(System.in);
         int a=0;
         do {
-            System.out.println("Press '1' for adding a data\tPress '2' for removing a data\tPress '3' for display the list: ");
+            System.out.println("Press '1' for adding a data\nPress '2' for removing a data\nPress '3' for display the list\nPress '4' to display in reverse\nPress '5' to search an element\nPress '6' to check the list size\nPress '7' to Get the First and Last element from the list\nPress '8' to get the Middle element from the list\nPress '9' to clear the list: ");
             int ch=sc.nextInt();
             switch (ch) {
                 case 1: System.out.println("Enter a data to add to the list: ");
@@ -84,11 +163,33 @@ public class LinkedListPractice_2 {
                 case 3: System.out.println("The list is : ");
                         linkedList.display();
                 break;
+
+                case 4: System.out.println("The reverse list is: ");
+                        linkedList.reversePrint();
+                        linkedList.display();
+                break;
+
+                case 5: System.out.println("Enter an element to search: ");
+                        int search=sc.nextInt();
+                        linkedList.searchElement(search);
+                break;
                 
+                case 6: linkedList.measureSize();
+                break;
+
+                case 7: linkedList.getFirstAndLastElement();
+                break;
+
+                case 8: linkedList.middleElement();
+                break;
+
+                case 9: linkedList.clearList();
+                break;
+
                 default: System.out.println("Please choose the coorect one.");
                 break;
             }
-            System.out.println("Press 1 to do again: ");
+            System.out.println("Press 1 to do again & Press 0 to exit from the program: ");
             a=sc.nextInt();
         } while (a==1);
         sc.close();
