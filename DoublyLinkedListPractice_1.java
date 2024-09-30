d88fd9e6ca3c6de8ae29cc8da64eb18c92be934d
Kundu-A <kunduarpan43@gmail.com>
2024-09-30 21:17:42 +0530

    Some methods are added

--- DoublyLinkedListPractice_1.java
+++ DoublyLinkedListPractice_1.java
@@ -12,6 +12,7 @@ class Node{
 class DoublyLinkedList{
     Node head;
     Node tail;
+    Scanner sc=new Scanner(System.in);
     public DoublyLinkedList(){
         head=null;
         tail=null;
@@ -64,6 +65,85 @@ class DoublyLinkedList{
         }
         System.out.println();
     }
+    public void backwardDisplay(){
+        if (head==null) {
+            System.out.println("The List is empty.");
+            return;
+        }
+        Node temp=head;
+        while (temp.next!=null) {
+            temp=temp.next;
+        }
+        while (temp!=null) {
+            System.out.print(temp.data+" ");
+            temp=temp.previous;
+        }
+        System.out.println();
+    }
+    public void searchElement(){
+        if (head==null) {
+            System.out.println("The List is empty.");
+            return;
+        }
+        Node temp=head;
+        System.out.println("Enter a value to search in the list: ");
+        int value=sc.nextInt();
+        int c=0,flag=0;
+        while (temp!=null) {
+            c++;
+            if (temp.data==value) {
+                System.out.println("The value is present at "+c+" position in the list.");
+                flag=1;
+                break;
+            }
+            temp=temp.next;
+        }
+        if (flag==0) {
+            System.out.println("The list is not present.");
+        }
+    }
+    public void insertAtBegining(int data){
+        Node newNode=new Node(data);
+        Node temp=head;
+        newNode.next=temp;
+        head.previous=newNode;
+        head=newNode;
+    }
+    public void positionalInsert(){
+        if (head==null) {
+            System.out.println("The list is empty.");
+            return;
+        }
+        System.out.println("Enter a data to insert into a specific position: ");
+        int data=sc.nextInt();
+        Node newNode = new Node(data);
+        System.out.println("Enter that specific position: ");
+        int pos=sc.nextInt();
+        if (pos==1) {
+            insertAtBegining(data);
+            return;
+        }
+        Node temp=head;
+        int lSize=0;
+        while (temp!=null) {
+            lSize++;
+            temp=temp.next;
+        }
+        if (pos>lSize) {
+            System.out.println("The List size is "+lSize+" but the provided position is "+pos);
+            return;
+        }
+        temp=head;
+        int count=1;
+        while (temp!=null && count<pos-1) {
+            count++;
+            temp=temp.next;
+        }
+        newNode.next=temp.next;
+        newNode.previous=temp;
+        temp.next.previous=newNode;
+        temp.next=newNode;
+    }
 }
 public class DoublyLinkedListPractice_1 {
     public static void main(String[] args) {
@@ -71,7 +151,7 @@ public class DoublyLinkedListPractice_1 {
         Scanner sc=new Scanner(System.in);
         int loop=0;
         do {
-            System.out.println("Press '1' to Add Node:\nPress '2' to remove:\nPress '3' to display in a forward way:");
+            System.out.println("Press '1' to Add Node:\nPress '2' to remove:\nPress '3' to display in a forward way:\nPress '4' to display in backward way:\nPress '5' to search an element:\nPress '6' to insert value at begining\nPress '7' to insert any value position-wise:");
             int ch=sc.nextInt();
             switch (ch) {
                 case 1: System.out.println("Enter a data to add: ");
@@ -85,6 +165,20 @@ public class DoublyLinkedListPractice_1 {
                 break;
 
                 case 3: doublyLinkedList.forwardDisplay();
+                break;
+
+                case 4: doublyLinkedList.backwardDisplay();
+                break;
+
+                case 5: doublyLinkedList.searchElement();
+                break;
+
+                case 6: System.out.println("Enter a value to insert at the begining of the list: ");
+                        int beginData=sc.nextInt();
+                        doublyLinkedList.insertAtBegining(beginData);
+                break;
+
+                case 7: doublyLinkedList.positionalInsert();
                 break;
 
                 default: System.out.println("Please choose the right option: ");
